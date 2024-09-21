f0add789c5a52b263bc74f95ff42cba1962cb151
huqiwei123 <463564148@qq.com>
2024-09-21 17:32:03 +0800

    第一次提交

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) {
+	  System.out.println("init local repository");
+    }
+}
---
+++ src/main/java/com/test/Sub.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Sub {
+    public static void main(String[] args) {
+	  System.out.println("init local repository");
+    }
+}
